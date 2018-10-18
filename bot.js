bfa20a9ced6d6d46c98e924d6f60b14eab59d39a
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-18 13:35:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -250,31 +250,5 @@ client.on('message', message => {
   }
  });
 
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'soft');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", ".soft network")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
 
 client.login(process.env.BOT_TOKEN2);
