6420bb8bc76e1c38b196382b4495ae0d23932dd7
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-18 16:12:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,24 +19,6 @@ client.on('ready', () => { //console.log
   });
 
  
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
-
-message.author.send(`**مدة الرابط : يـوم
-عدد استخدامات الرابط : 100**`)
-
-
-    }
-});
 
 client.on('message',async message => {
     if(message.content.startsWith(prefix + "setVoice")) {
@@ -246,6 +228,36 @@ client.on('ready', () => {
         channel.join();
     } else {
         console.log('...');
+    }
+});
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+ 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
+      message.author.sendEmbed(Embed11)
     }
 });
 client.login(process.env.BOT_TOKEN2);
