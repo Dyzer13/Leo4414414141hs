5a1085af4e0d3987f03b59c1873ce4de0a1f4267
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-18 16:42:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -396,22 +396,5 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
- if(message.content.startsWith(prefix + "تعال")) {
-message.member.voiceChannel.join();
-}
-});
-
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "soft");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
-});
 
 client.login(process.env.BOT_TOKEN2);
