e284b610efdcb2492419e624e8dbd14f2f24b2d7
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-21 22:48:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,4 +116,22 @@ client.on("guildMemberAdd", (member) => {
     });
 });
 
+client.on("message", message => {
+    var prefix = "."
+ const embed = new Discord.RichEmbed() 
+      .setColor("#ffff00")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(`
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "mcskin") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send(" Type your skin name ");
+        const image = new Discord.Attachment(https://minotar.net/armor/body/${args}, "skin.png");
+    message.channel.send(image)
+        }
+
+message.author.sendEmbed(embed)
+    });
 client.login(process.env.BOT_TOKEN2);
