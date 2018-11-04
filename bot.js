fccd946e401d7869480f486e5691ff29268aa053
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-04 23:16:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '*'
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['1kkk','.Soft Network'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [`.help ON ${client.guilds.size} Servers`,`.help ${client.users.size} Users`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/clo_wiin`);
+    }, ms);
+
 });
+
  
 
 client.on('message',async message => {
