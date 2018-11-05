a3c959d938529ae8a6465a59c28055697743e767
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-05 20:27:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,21 @@
-const Discord = require('discord.js');
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`on 2 servers | *help |
+});
+client.on('message', message => {
+     if (message.content === ".servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
