a33048c66947ac46e82277647414c73a60cfd5fa
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-05 20:14:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,39 +31,16 @@ client.on('message',async message => {
 });
 
 
-module.exports.run = async (bot, message, args) => {
-    message.delete();
-    if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
-    if(args[0] == "help"){
-      message.reply("Usage: !ban <user> <reason>");
-      return;
-    }
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return errors.cantfindUser(message.channel);
-    if(bUser.id === bot.user.id) return errors.botuser(message); 
-    let bReason = args.join(" ").slice(22);
-    if(!bReason) return errors.noReason(message.channel);
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "MANAGE_MESSAGES");
-
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
-    .setColor("#bc0000")
-    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
-    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Banned In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", bReason);
-
-    let incidentchannel = message.guild.channels.find(`name`, "incidents");
-    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(bUser).ban(bReason);
-    incidentchannel.send(banEmbed);
-}
-
-module.exports.help = {
-  name:"ban"
-}
-
+var prefix = "$";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 
 client.login(process.env.BOT_TOKEN2);
