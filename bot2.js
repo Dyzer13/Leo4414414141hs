42495fd77ae3d08475f4bfca018999720840f7ed
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-06 21:06:43 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ''
+const prefix = '*'
 
 client.on('ready', () => { //console.log
   console.log('--------------------------');
@@ -19,7 +19,7 @@ client.on('ready', () => { //console.log
  
   });
 client.on('ready', () => { //playing
-    client.user.setGame(`#Venom 5k`,'https://www.twitch.tv/clo_wiin');
+    client.user.setGame(`.SoftNetwork`,'https://www.twitch.tv/clo_wiin');
     client.user.setStatus('Online')
 });
 
@@ -31,5 +31,40 @@ setTimeout(() => {
 },1500);
 
 });
-
+client.on('message',async message => {
+  if(message.author.bot || message.channel.type === 'bc') return;
+  let args = message.content.split(' ');
+  if(args[0] === `${prefix}bc`) {
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+    if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+  
+    let msgCount = 0;
+    let errorCount = 0;
+    let successCount = 0;
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
+      message.guild.members.forEach(g => {
+        g.send(args.slice(1).join(' ')).then(() => {
+          successCount++;
+          msgCount++;
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+        }).catch(e => {
+          errorCount++;
+          msgCount++;
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+        });
+      });
+    });
+  }
+});
+var prefix = "*";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 client.login(process.env.BOT_TOKEN);
