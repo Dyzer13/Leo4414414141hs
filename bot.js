760896f7bdcfaeb4fb6077c3e786ed979b08336a
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-09 23:31:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -672,4 +672,25 @@ let args = message.content.split(" ").slice(1);
 	     }
   });
 
+
+   client.on("message", message => {
+    var prefix = ".";
+
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "image"){
+          const embed = new Discord.RichEmbed()
+
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
+
+
 client.login(process.env.BOT_TOKEN);
