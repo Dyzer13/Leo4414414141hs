cfb8be355774ecab84466124d8154348ae86ea8c
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-09 23:47:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,8 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const convert = require("hh-mm-ss")
-const dateFormat = require('dateformat');
-const fs = require('fs');
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-const userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
-const moment = require('moment');
-var jimp = require('jimp')
-const prefix = ".";
-let done = {};
+const prefix = "-";
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
