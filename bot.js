e6f27a0773df15f03949316b9d598ef5f3d74eb5
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-09 23:17:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -418,20 +418,7 @@ message.channel.sendEmbed(embed);
     }
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 
 client.login(process.env.BOT_TOKEN);
