40d03b78af3290f750c19ecf00f35d050c796270
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-09 23:41:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1111,4 +1111,17 @@ reaction2.on("collect", r => {
 });
 
 
+client.on('message', message => {
+       if (message.content.startsWith(prefix + 'vpsbot')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#00ff47");
+  message.channel.send(embed);
+}
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
