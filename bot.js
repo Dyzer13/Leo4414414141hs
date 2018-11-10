3dadb602653134b452996a80f73ad5a0556a6f7f
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-10 02:46:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1150,4 +1150,42 @@ let args = message.content.split(' ').slice(1).join(' ');
 }
 });
 
+client.on('message', ( message ) => {
+  if(message.author.bot) return;
+
+  if(message.channel.id !== '510204289184956418') return;
+
+
+  let types = [
+    'jpg',
+    'jpeg',
+    'png'
+  ]
+
+  if (message.attachments.size <= 0) {
+    message.delete();
+    message.channel.send(`${message.author}, This channel for Pic 🖼️ Only`) // 
+    .then(msg => {
+      setTimeout(() => {
+        msg.delete();
+      }, 5000)
+  })
+  return;
+}
+
+  if(message.attachments.size >= 1) {
+    let filename = message.attachments.first().filename
+    console.log(filename);
+    if(!types.some( type => filename.endsWith(type) )) {
+      message.delete();
+      message.channel.send(`${message.author}, This channel for Pic 🖼️ Only`)
+      .then(msg => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000)
+      })
+    }
+  }
+
+});
 client.login(process.env.BOT_TOKEN);
