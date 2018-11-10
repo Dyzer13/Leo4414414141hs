b0bf745585ea3c78f5bef6b5b2569cc44986ca9e
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-10 12:48:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1307,27 +1307,6 @@ Servers Counter : __${client.guilds.size}__**`)
 
 );
 
-client.on('guildMemberAdd', member => {
-        let channel = member.guild.channels.find('name', 'soft');
-        let memberavatar = member.user.avatarURL
-          if (!channel) return;
-        let embed = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setThumbnail(memberavatar)
-            .addField('🎽 | name :  ',`${member}`)
-            .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
-            .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
-                    .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-                   
-                      .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                         
-                                         .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                           
-         .setFooter(`${member.guild.name}`)
-            .setTimestamp()
-       
-          channel.sendEmbed(embed);
-        });
 
 Client.on('message', message => {
 
