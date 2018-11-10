882003a2b7941053345196339b087057040e05ae
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-10 12:40:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1307,18 +1307,6 @@ Servers Counter : __${client.guilds.size}__**`)
 
 );
 
-client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTimestamp()
-            .setTitle('``رساله جديده في خاص البوت``')
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
-        client.channels.get("510761434838335498").send({ embed: stewart });
-    }
-});
+
 
 client.login(process.env.BOT_TOKEN);
