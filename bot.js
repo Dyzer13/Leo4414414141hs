e956699f2e6769531bb7cbdae3c797f2c87f28b9
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-10 20:55:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,18 +23,18 @@ client.on('message', message => {
     if (message.content.includes('discord.gg')){
                         if(!message.channel.guild) return message.reply ('')
                     if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
-       message.channel.send('ban <@' + message.author.id + '>')
+       message.channel.send('mute <@' + message.author.id + '>')
        message.delete() 
        }
     }
-          if (message.content.startsWith("ban ")) {
+          if (message.content.startsWith("mute ")) {
              if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
              var member= message.mentions.members.first();
              member.ban().then((member) => {
                  message.channel.sendMessage("", {embed: {
                  author: {
                  },
-                 title: 'بسبب النشر ' + member.displayName + ' تم حظر',
+                 title: 'بسبب النشر ' + member.displayName + ' تم اعطائك ميوت',
                  color: 490101,
                  }
                });
