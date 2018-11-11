68175969b29f0fae89126e2da3a99180dc2e3d55
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-11 22:55:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,26 +38,6 @@ m.sendMessage(args)
 
 
 
-client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
-    const embed500 = new Discord.RichEmbed()
-      .setTitle("❌ | تمت معاقبتك")
-            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL) 
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500) 
-    
-        
-    }
-    }
-})
 
 
 client.on('message', message => {
@@ -87,7 +67,7 @@ client.on('message', message => {
           // An error happened
           // This is generally due to the bot not being able to kick the member,
           // either due to missing permissions or role hierarchy
-          message.reply('I was unable to kick the member');
+          message.reply('**I was unable to kick the member');
           // Log the error
           console.error(err);
         });
