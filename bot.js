5709024c4de4cab86decd6cfa6d7d9e99ad79d42
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-12 23:32:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', message => {
+client.on('message', message => {
 
     // Variables - Variables make it easy to call things, since it requires less typing.
     let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
