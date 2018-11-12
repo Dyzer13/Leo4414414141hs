7b1500379d66f6da4be8cf25cffb0f3ff647ac3e
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-13 00:13:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,25 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = "$";
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers | $help`);
 });
 
+client.on("guildCreate", guild => {
+  //  when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers | $help`);
+});
+
+client.on("guildDelete", guild => {
+  // when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers | $help`);
+});
 client.on('message', message => {
 
     // Variables - Variables make it easy to call things, since it requires less typing.
