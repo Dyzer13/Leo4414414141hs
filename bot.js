34a59f206299c545daf102ae0e2e25ae8f0e3d4f
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-13 00:36:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,18 @@ client.on("guildDelete", guild => {
   client.user.setActivity(`Serving ${client.guilds.size} servers | $help`);
 });
 
+if(message.channel.type === "dm") {
+    let embed = new Discord.RichEmbed()
+    .setTimestamp()
+    .setTitle("Direct Message To The Bot")
+    .addField(`Sent By:`,`<@${message.author.id}>`)
+    .setColor("RANDOM")
+    .setThumbnail(message.author.displayAvatarURL)
+    .addField(`Message: `,message.content)
+    .setFooter(`DM Bot Messages | DM Logs`)
+   
+    client.users.get("506996140898648074").send(embed)
+  }
 
 
 client.on('message', async message => {
