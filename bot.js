0e8f00db780dc0e941618e2cb2015a5329714b9c
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-13 01:44:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -360,31 +360,6 @@ client.on('message', message => {
 });
 
 
-client.on('message',async message => {
-    if(message.author.bot || message.channel.type === '.bc') return;
-    let args = message.content.split(' ');
-    if(args[0] === `${prefix}bc`) {
-      if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- You do not have the necessary permissions to use this command');
-      if(!args[1]) return message.channel.send('- You must type the message after the command');
-    
-      let msgCount = 0;
-      let errorCount = 0;
-      let successCount = 0;
-      message.channel.send(`- [ :bookmark: :: ${msgCount} ] ・Number of messages sent \n- [ :inbox_tray: :: ${successCount} ] ・Number of messages received\n- [ :outbox_tray: :: ${errorCount} ]・Number of messages not received`).then(msg => {
-        message.guild.members.forEach(g => {
-          g.send(args.slice(1).join(' ')).then(() => {
-            successCount++;
-            msgCount++;
-            msg.edit(`- [ :bookmark: :: ${msgCount} ] ・Number of messages sent \n- [ :inbox_tray: :: ${successCount} ] ・Number of messages received\n- [ :outbox_tray: :: ${errorCount} ]・Number of messages not received`);
-          }).catch(e => {
-            errorCount++;
-            msgCount++;
-            msg.edit(`- [ :bookmark: :: ${msgCount} ] ・Number of messages sent \n- [ :inbox_tray: :: ${successCount} ] ・Number of messages received\n- [ :outbox_tray: :: ${errorCount} ]・Number of messages not received`);
-          });
-        });
-      });
-    }
-  });
   
   client.on("message", msg => {
 if(msg.content.startsWith (prefix + "id")) {
@@ -851,7 +826,7 @@ client.on('message', msg => {
                       })
                     }
                     
-
+});
 
 
 
