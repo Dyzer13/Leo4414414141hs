13231b8a6bdfac97869c16f1b83659e3a10e7178
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-13 20:33:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -583,6 +583,19 @@ client.on('messageUpdate', (message) => {
 
 
 
+ if (msg.startsWith(prefix + 'purge')) {
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No")
+ 
+     if(!args[0]) return message.channel.send("Please provide a number.");
+     if(args[2]) return message.channel.send(" \`\`\`A unknown error has occured\`\`\`")
+ 
+    message.channel.bulkDelete(args[0]).then(() => {
+ 
+     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+ 
+   });
+   }
 
 
 
