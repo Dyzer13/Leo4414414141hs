6688c48bc2380ec13507dfe12480240cf7ae6092
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-12-20 23:53:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "-";
 
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
- 
+
   message.channel.createInvite({
         thing: true,
         maxUses: 5,
@@ -12,46 +12,38 @@ client.on('message', message => {
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
- هذا الرابط صالح لمده 24 ساعه فقط ولخمسة اشخاص فقط
- ${message.guild.name}   رابط
-**`)
-      message.author.sendEmbed(Embed11)
+  message.channel.send("**:link:.تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 5**`)
+
+
     }
-});
 
 client.on('message', message => {
     let argresult = message.content.split(` `).slice(1).join(' ');
-    if (message.content.startsWith(prefix + 'setStreaming')) {
+    if (message.content.startsWith('-setStreaming')) {
       if (!devs.includes(message.author.id)) return message.channel.send("<@274160618448093185> only this guy can do restart the bot so don't try again :wink:.");
       message.delete();
       client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
 
-    } else if(message.content.startsWith(prefix + 'setWatching')) {
+    } else if(message.content.startsWith('-setWatching')) {
         client.user.setActivity(argresult,{type: 'WATCHING'});
 
-      } else if(message.content.startsWith(prefix + 'setListening')) {
+      } else if(message.content.startsWith('-setListening')) {
         client.user.setActivity(argresult,{type: 'LISTENING'});
 
-      } else if(message.content.startsWith(prefix + 'setPlaying')) {
+      } else if(message.content.startsWith( 'setPlaying')) {
         client.user.setActivity(argresult,{type: 'PLAYING'});
 
-      } else if(message.content.startsWith(prefix + 'setName')) {
+      } else if(message.content.startsWith('-setName')) {
         client.user.setUsername(argresult);
 
-      } else if(message.content.startsWith(prefix + 'setAvatar')) {
+      } else if(message.content.startsWith('-setAvatar')) {
         client.user.setAvatar(argresult);
 
 
-      } else if(message.content.startsWith(prefix + 'setStatus')) {
+      } else if(message.content.startsWith('-setStatus')) {
         if(!argresult) return message.channel.send('`online`, `DND(Do not Distrub),` `idle`, `invisible(Offline)` :notes: أختر أحد الحالات');
         client.user.setStatus(argresult);
 
